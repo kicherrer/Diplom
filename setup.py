7b6dae6cf0ef34634ba6afa43177da4953d5b1fd
Иван Вергейчик <ivanvergejcik@MacBook-Air-Ivan.local>
2025-02-24 01:04:59 +0300

    Settings project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="diplom",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # requirements will be read from requirements.txt
+    ],
+)
